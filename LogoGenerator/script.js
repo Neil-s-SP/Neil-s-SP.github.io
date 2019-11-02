3ac0e829466c3c23ae28d0b61272329fc0514ec7
neilzhu2 <neilzhu@umich.edu>
2019-11-02 00:32:38 -0700

    show all logos

--- LogoGenerator/script.js
+++ LogoGenerator/script.js
@@ -2,67 +2,125 @@
 
 
 //Get the window's width and hight
-    var windowWidth = window.innerWidth,
-        windowHeight = window.innerHeight;
-    var svgWidth = windowWidth * 0.8,
-        svgHeight_list = windowHeight * 0.8,
-        svgHeight_logos = windowHeight * 0.3,
-        svgHeight_result = windowHeight * 0.5;
+    const windowWidth = window.innerWidth,
+            windowHeight = window.innerHeight;
+    const svgWidth = windowWidth * 0.8,
+            svgHeight_list = windowHeight * 0.8,
+            svgHeight_logos = windowHeight * 0.3,
+            svgHeight_result = windowHeight * 0.5;
+    const uploadedLogoHeight = svgHeight_logos * 0.3;
 
-    console.log(svgHeight_result);
 
 
 
 // Get the data of the recording JSON file
 
     d3.json("./storage/record.json").then(function(data){
-        events = data.events;
-        console.log(events);
-        // CREATE the list of events at #eventsSVG
-            const eventsSVG = d3.select("#eventsSVG")
-                                .attr("width", svgWidth)
-                                .attr("height", svgHeight_list);
-            var eventItems = eventsSVG.selectAll(".eventList")
-                                        .data(events)
-                                        .enter()
-                                        .append("g")
-                                        .attr("class", "eventList")
-                                        .attr("id", (d) => `event_${d.name}`)
-                                        // .on("click", function)
+        var events = data.events;
 
-            eventItems.append("rect")
-                        .attr("x", svgWidth * 0.1)
-                        .attr("y", (d) => 10 + svgHeight_list * 0.125 * d.id)
-                        .attr("width", svgWidth * 0.8)
-                        .attr("height", svgHeight_list * 0.1)
-                        .style("fill", "white")
-                        .style("stroke", "black")
-                        .style("stroke-width", 1)
-
-            eventItems.append("text")
-                        .attr("x", svgWidth * 0.2)
-                        .attr("y", (d) => 10 + svgHeight_list * 0.125 * d.id + svgHeight_list * 0.06)
-                        .text((d) => d.name)
-                        .style("fill", "black")
+        generateEventList(events)
 
 
     });
 
 
+
 // CREATE the list of events at #eventsSVG
+    const generateEventList = (eventsData) => {
+        var eventsSVG = d3.select("#eventsSVG")
+                            .attr("width", svgWidth)
+                            .attr("height", svgHeight_list);
+        var eventItems = eventsSVG.selectAll(".eventList")
+                                    .data(eventsData)
+                                    .enter()
+                                    .append("g")
+                                    .attr("class", "eventList")
+                                    .attr("id", (d) => `event_${d.name}`)
+                                    .on("mouseover", function(){eventItemColorChange(this, "grey");})
+                                    .on("mouseout", function(){ eventItemColorChange(this, "white");})
+                                    .on("click", (d) => {
+                                        changePage("eventList", "logoGenerator");
+                                        initiateLogoGenerator(d);
+                                    })
+
+        eventItems.append("rect")
+                    .attr("x", svgWidth * 0.1)
+                    .attr("y", (d) => 10 + svgHeight_list * 0.125 * d.id)
+                    .attr("width", svgWidth * 0.8)
+                    .attr("height", svgHeight_list * 0.1)
+                    .style("fill", "white")
+                    .style("stroke", "black")
+                    .style("stroke-width", 1)
+
+        eventItems.append("text")
+                    .attr("x", svgWidth * 0.2)
+                    .attr("y", (d) => 10 + svgHeight_list * 0.125 * d.id + svgHeight_list * 0.06)
+                    .text((d) => d.name)
+                    .style("fill", "black")
+    }
+
+    // When one item is hovered or hovernoted, the color of its rec will change
+        const eventItemColorChange = (thisEvent, targetColor) => {
+            d3.select(thisEvent)
+                .select("rect")
+                .style("fill", targetColor)
+        }
 
     // When one item is selected: #eventList will be hided, and #logoGenerator will be unhided
+        const changePage = (earlierPageId, nextPageId) => {
+            var earlierPageSection = document.querySelector(`#${earlierPageId}`),
+                nextPageSection = document.querySelector(`#${nextPageId}`);
 
+            earlierPageSection.classList.add("hide");
+            nextPageSection.classList.remove("hide");
+        }
 
 
 //INITIATE #logoGenerator
-
-    // Initiate the #logoGenerator according to the logos and presets
-
-
+    const initiateLogoGenerator = (eventData) => {
+        showUploadedLogos(eventData);
+    }
 
     // Initiate the #logosSVG according to the logos based on the recording JSON file
+        const showUploadedLogos = (eventData) => {
 
+            console.log("showUploadedLogos used");
+            var logoNames = eventData.logos,  // logoNames: ["1", "2", ...]
+                eventName = eventData.name;
+
+            var uploadedLogosDiv = d3.select("#uploadedLogos");
+
+            var logoDivs = uploadedLogosDiv.selectAll(".uploadedLogoDiv")
+                                            .data(logoNames)
+                                            .enter()
+                                            .append("div")
+                                            .attr("class", "uploadedLogoDiv")
+                                            .attr("id", (d) => `logo_${d}`);
+
+            logoDivs.append("img")
+                    .attr("src", (d) => `./storage/${eventName}/${d}.png`);
+
+            logoDivs.append("p")
+                    .text((d) => convertNumToLetter(Number(d)));
+
+
+            //Create a button for adding img
+                uploadedLogosDiv.append("div")
+                                .attr("class", "uploadedLogoDiv")
+                                .attr("id", "uploadButton")
+                                .style("margin-left", "2em")
+                                .append("img")
+                                .attr("src", "./assets/uploadLogo.png")
+                                .style("height", "4em")
+                                .on("mouseover", function() {d3.select(this).attr("src", "./assets/uploadLogo_hover.png")})
+                                .on("mouseout", function() {d3.select(this).attr("src", "./assets/uploadLogo.png")});
+
+
+
+
+
+
+        }
 
 
     // Initiate the value of #typefield_sequence based on the recording JSON file
@@ -101,3 +159,13 @@
 
 
     // When a value of .typefield_Nums is changed, update the #typefield_Num_last, update the recording JSON file, regenerate the #quickReviewSVG accordingly
+
+
+
+
+
+
+//AUXILIARY FUNCTIONS
+    const convertNumToLetter = (num) => {
+        return String.fromCharCode(num+64);
+    }
